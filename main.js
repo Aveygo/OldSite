85867ef9eb2626921742107179740e3fbe43af7a
Aveygo <dmitri.shevchenko.au@gmail.com>
2023-03-26 15:38:06 +1100

    cool tile effects

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-var rain_amount = 100;                // Number of rain drops, default=100
+var rain_amount = 100;
 
-const isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+var isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+var isMobile = /Mobi|Android/i.test(navigator.userAgent);
+var should_tilt = false;
+
+var tiles = document.getElementsByClassName('tile');
 
 function draw_rain() {
     // Rain initialization
-
-    // check screen width or mobile
-
-    if (/Mobi|Android/i.test(navigator.userAgent)) {
-        rain_amount *= 0.1;
-        console.log("Mobile detected, spawned only " + rain_amount + " drops");
-    } else if (window.innerWidth < 500) {
-        rain_amount *= 0.1;
-        console.log("Very small screen detected, spawned only " + rain_amount + " drops");
-    } else if (window.innerWidth < 800) {
-        rain_amount *= 0.5;
-        console.log("Small screen detected, spawned only " + rain_amount + " drops");
-    }
 
     for (let i = 0; i < rain_amount; i++) {
         
@@ -77,27 +68,112 @@ function setup_super_unsafe_css() {
     // sets image-rendering to pixelated after page load
     window.addEventListener('load', function () {
         document.getElementsByClassName("background")[0].style.imageRendering = "pixelated";
-        /*
-        var tiles = document.getElementsByClassName("tile");
-        for (var i = 0; i < tiles.length; i++) {
-            tiles[i].style.minWidth = "min(100%, 600px)";
-        }
-        var small_tiles = document.getElementsByClassName("small_tile");
-        for (var i = 0; i < small_tiles.length; i++) {
-            small_tiles[i].style.minWidth = "min(100%, 200px)";
-        }
-        */
-
     });
 }
 
+function setup_tile_borders() {
+    document.addEventListener('mousemove', function(e) {
+        if (should_tilt) {
+            for (let tile of tiles) {
+                let rect = tile.getBoundingClientRect()
+                let x = e.clientX - rect.left
+                let y = e.clientY - rect.top;
+                tile.style.setProperty("--mouse-x", `${x}px`);
+                tile.style.setProperty("--mouse-y", `${y}px`);
+            };
+        }
+    });
+
+    document.addEventListener('mouseleave', function(e) {
+        if (should_tilt) {
+            for(let tile of tiles) {
+                tile.style.setProperty("--mouse-x", `${0}px`);
+                tile.style.setProperty("--mouse-y", `${-1000}em`);
+            };
+        }
+    });
+}
+
+function setup_tile_tilts() {
+
+    function calculate_tilt(tile, e) { 
+        const rect = tile.getBoundingClientRect();
+        const x_center = rect.left + rect.width / 2;
+        const y_center = rect.top + rect.height / 2;
+
+        const dx = e.clientX - x_center;
+        const dy = e.clientY - y_center;
+
+        var rotate_x = 2 * dy / (rect.height / 2);
+        var rotate_y = -2 * dx / (rect.width / 2);
+        tile.style.transition = 'none';
+        tile.style.transform = `perspective(1000px) rotateX(${rotate_x}deg) rotateY(${rotate_y}deg)`;
+    }
+
+    for (var i = 0; i < tiles.length; i++) {
+        let tile = tiles[i];
+
+        tile.classList.remove("fly_in");
+
+        tile.addEventListener('mousemove', function(e) {
+            if (should_tilt) {
+                calculate_tilt(tile, e);
+            }
+        });
+
+        tile.addEventListener('mouseleave', (e) => {
+            tile.style.transition = 'all 1s ease';
+            tile.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
+        });
+    }
+}
+
+function calculate_rain_amount() {
+    if (isMobile) {
+        rain_amount *= 0.1;
+        console.log("Mobile detected, spawned only " + rain_amount + " drops");
+    } else if (window.innerWidth < 500) {
+        rain_amount *= 0.1;
+        console.log("Very small screen detected, spawned only " + rain_amount + " drops");
+    } else if (window.innerWidth < 800) {
+        rain_amount *= 0.5;
+        console.log("Small screen detected, spawned only " + rain_amount + " drops");
+    }
+}
+
 async function main() {
+    
     if (!isReduced) {
+        calculate_rain_amount();
         draw_rain();
         setup_parallax();
         setup_easter_egg();
+        
+        if (!isMobile) {
+            should_tilt = true;
+            setup_tile_borders();
+            // wait for for initial tile fly in
+            setTimeout(function(){                
+                setup_tile_tilts();
+            }, 1000);
+            
+        }
+
     }
+
     setup_super_unsafe_css();
 }
 
 main()
+
+window.onresize = function() {
+    var isReduced = window.matchMedia(`(prefers-reduced-motion: reduce)`) === true || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+    var isMobile = /Mobi|Android/i.test(navigator.userAgent);
+    if (isReduced || isMobile) {
+        console.log("Resized - should not tilt!");
+        should_tilt = false;
+    } else {
+        console.log("Resized - should tilt!");
+        should_tilt = true;
+    }
+}
